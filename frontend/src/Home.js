0115f80e2dc30c5f59531303c92156e12d3c5b22
lukaslinares <108452712+lukaslinares@users.noreply.github.com>
2023-03-23 21:53:11 -0700

    Lukas is home

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,7 +2,7 @@
 function Home() {
   return (
     <main>
-      <h1>HOME</h1>
+      <h1>Lukas is HOME</h1>
       <div >
         <img height="300" width="500" src={process.env.REACT_APP_SERVER_URL + 'images/chia-fruit-drink.jpg'} alt="Chia Fruit Shake" />
         <div>
